d3d82a2582f1aaa29a751e0c025f27bccfc7ab82
bsh-ko <ssuh3872@gmail.com>
2024-09-29 23:01:14 +0900

    다중 선택 가능한 상품리스트 표시

---
+++ main.js
@@ -0,0 +1,11 @@
+function Product(name, price){ 
+    this.name = name 
+    this.price = price 
+} 
+   
+let products = [
+     new Product('대뱃살', 3000),
+      new Product('목살', 5000), 
+      new Product('배꼽살', 4000),
+      new Product('중뱃살', 1000) 
+]
